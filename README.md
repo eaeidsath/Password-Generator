6971533d9a8c53fbfa05f2db10864b606e0b6b9b
unknown <evelyneidsath@gmail.com>
2023-11-11 00:52:38 -0800

    define password variables, make password generation prompts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Password Generator Starter Code
+
+
+source referenced for converting array to uppercase: https://stackoverflow.com/questions/29719329/convert-array-into-upper-case
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = lowerCase.map(function(x){return x.toUpperCase(); });
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "_", "-", "+", "=", "/"];
 
+function customize() {
+  var userLowerCase = window.confirm("Include lowercase letters?");
+  var userUpperCase = window.confirm("Include upper case letters?");
+  var userNumbers = window.confirm("Include numbers?");
+  var userSpecialCharacters = window.confirm("Include special characters?")
+}
+
+function generatePassword() {
+  var userLength = prompt("Enter desired length of password between 8 and 128 characters.");
+  if ((userLength < 8) || (userLength > 128)) {
+    window.alert("Password length must be between 8 and 128 characters.");
+    var startOver = window.confirm("Start over?");
+    if (startOver) {generatePassword();};
+  } else if ((userLength > 8) && (userLength < 128)) {
+    customize();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
