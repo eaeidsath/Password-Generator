89a233a58cbfb51fd91dbf55273ab5703b9e7056
unknown <evelyneidsath@gmail.com>
2023-11-11 02:16:31 -0800

    write code to randomize passwords, begin permutations

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 source referenced for converting array to uppercase: https://stackoverflow.com/questions/29719329/convert-array-into-upper-case
+
+pulling more than one random element from an array: https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,7 @@ var upperCase = lowerCase.map(function(x){return x.toUpperCase(); });
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "_", "-", "+", "=", "/"];
 
-function customize() {
-  var userLowerCase = window.confirm("Include lowercase letters?");
-  var userUpperCase = window.confirm("Include upper case letters?");
-  var userNumbers = window.confirm("Include numbers?");
-  var userSpecialCharacters = window.confirm("Include special characters?")
-}
+
 
 function generatePassword() {
   var userLength = prompt("Enter desired length of password between 8 and 128 characters.");
@@ -18,7 +13,40 @@ function generatePassword() {
     var startOver = window.confirm("Start over?");
     if (startOver) {generatePassword();};
   } else if ((userLength > 8) && (userLength < 128)) {
-    customize();
+    var userLowerCase = window.confirm("Include lowercase letters?");
+    var userUpperCase = window.confirm("Include upper case letters?");
+    var userNumbers = window.confirm("Include numbers?");
+    var userSpecialCharacters = window.confirm("Include special characters?");
+  }
+  //function getRandomPassword() {
+  //  var randomPassword = lowerCase[Math.floor(Math.random()*lowerCase.length)];
+  //}
+  //getRandomPassword();
+
+  if ((userLowerCase === true) && (userUpperCase === true) && (userNumbers === true) && (userSpecialCharacters === true)) {
+    var n = userLength;
+    const allMerged = [...lowerCase, ...upperCase, ...numbers, ...specialCharacters];
+    const shuffled = allMerged.sort(function() {return 0.5 - Math.random(); });
+    var selected = shuffled.slice(0,n);
+    var result = selected.join("");
+    return result;
+  } else if ((userLowerCase === false) && (userUpperCase === true) && (userNumbers === true) && (userSpecialCharacters === true)) {
+    const upperNumSpecial = [...upperCase, ...numbers, ...specialCharacters];
+    const shuffled = upperNumSpecial.sort(function() {return 0.5 - Math.random(); });
+    var selected = shuffled.slice(0,n);
+    var result = selected.join("");
+    return result;
+  } else if ((userLowerCase === false) && (userUpperCase === false) && (userNumbers === true) && (userSpecialCharacters === true)) {
+    const numSpecial = [...numbers, ...specialCharacters];
+    const shuffled = numSpecial.sort(function() {return 0.5 - Math.random(); });
+    var selected = shuffled.slice(0,n);
+    var result = selected.join("");
+    return result;
+  } else if ((userLowerCase === false) && (userUpperCase === false) && (userNumbers === false) && (userSpecialCharacters === true)) {
+    const shuffled = specialCharacters.sort(function() {return 0.5 - Math.random(); });
+    var selected = shuffled.slice(0,n);
+    var result = selected.join("");
+    return result;
   }
 }
 
