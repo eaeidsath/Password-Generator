7c123c3eda2234717b1011ae12a0cf8cb9419bbf
unknown <evelyneidsath@gmail.com>
2023-11-17 00:53:11 -0800

    fix merged array length issue, update readme

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
 
 
-source referenced for converting array to uppercase: https://stackoverflow.com/questions/29719329/convert-array-into-upper-case
 
-pulling more than one random element from an array: https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
+## Installation
+
+N/A
+
+## Usage
+
+To view this webpage, enter the URL [https://eaeidsath.github.io/Password-Generator/](https://eaeidsath.github.io/Password-Generator/) into any browser.
+
+To use the password generator, click the 'Generate Password" button.
+
+When the prompt appears, type in the desired length of your password between 8 and 128 characters.
+
+The following 4 prompts will ask whether you want your password to include lowercase characters, uppercase characters, numbers, and special characters. On these prompts, click "Ok" to include the corresponding character type, or "Cancel" to not include the character type. You must select "Ok" to at least one of the character types in order to successfully generate a password.
+
+Once the prompts have been answered, the randomly generated password will appear in the text box.
+
+Copy the text in the box and paste it wherever you want to use your new password.
+
+If you enter a password length outside the bounds of 8-128, or if you do not select "Ok" to any of the character type prompts, you will be given an error message followed by prompt asking if you want to start over. Click "Ok" on this prompt to start the generation process again, or click "Cancel" to exit the prompt without generating a password.
+
+### Screenshots
+
+
+
+## Credits
+
+Evelyn Eidsath [https://github.com/eaeidsath](https://github.com/eaeidsath)
+
+Xander Rapstine [https://github.com/Xandromus](https://github.com/Xandromus)
+
+Tutorials:
+
+source referenced for converting array to uppercase (script.js line 3): [https://stackoverflow.com/questions/29719329/convert-array-into-upper-case](https://stackoverflow.com/questions/29719329/convert-array-into-upper-case)
+
+pulling more than one random element from an array (script.js lines 48-49, 56-57): [https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array](https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array)
--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ function generatePassword() {
     window.alert("Password length must be between 8 and 128 characters.");
     var startOver = window.confirm("Start over?");
     if (startOver) {generatePassword();};
-  } else if ((userLength > 8) && (userLength < 128)) {
+  } else if ((userLength >= 8) && (userLength <= 128)) {
     var userLowerCase = window.confirm("Include lowercase letters?");
     var userUpperCase = window.confirm("Include upper case letters?");
     var userNumbers = window.confirm("Include numbers?");
@@ -23,36 +23,6 @@ function generatePassword() {
     var startOver2 = window.confirm("Start over?");;
     if (startOver2) {generatePassword();};
   }
-  //function getRandomPassword() {
-  //  var randomPassword = lowerCase[Math.floor(Math.random()*lowerCase.length)];
-  //}
-  //getRandomPassword();
-
-/*   if ((userLowerCase === true) && (userUpperCase === true) && (userNumbers === true) && (userSpecialCharacters === true)) {
-    var n = userLength;
-    const allMerged = [...lowerCase, ...upperCase, ...numbers, ...specialCharacters];
-    const shuffled = allMerged.sort(function() {return 0.5 - Math.random(); });
-    var selected = shuffled.slice(0,n);
-    var result = selected.join("");
-    return result;
-  } else if ((userLowerCase === false) && (userUpperCase === true) && (userNumbers === true) && (userSpecialCharacters === true)) {
-    const upperNumSpecial = [...upperCase, ...numbers, ...specialCharacters];
-    const shuffled = upperNumSpecial.sort(function() {return 0.5 - Math.random(); });
-    var selected = shuffled.slice(0,n);
-    var result = selected.join("");
-    return result;
-  } else if ((userLowerCase === false) && (userUpperCase === false) && (userNumbers === true) && (userSpecialCharacters === true)) {
-    const numSpecial = [...numbers, ...specialCharacters];
-    const shuffled = numSpecial.sort(function() {return 0.5 - Math.random(); });
-    var selected = shuffled.slice(0,n);
-    var result = selected.join("");
-    return result;
-  } else if ((userLowerCase === false) && (userUpperCase === false) && (userNumbers === false) && (userSpecialCharacters === true)) {
-    const shuffled = specialCharacters.sort(function() {return 0.5 - Math.random(); });
-    var selected = shuffled.slice(0,n);
-    var result = selected.join("");
-    return result;
-  } */
 
   if (userLowerCase === true) {
     var mergedArraystep1 = [].concat(lowerCase);
@@ -75,20 +45,19 @@ function generatePassword() {
     var mergedArraystep4 = mergedArraystep3;
   };
   var n = userLength;
-  const shuffled = mergedArraystep4.sort(function() {return 0.5 - Math.random(); });
+  var shuffled = mergedArraystep4.sort(function() {return 0.5 - Math.random(); });
   var selected = shuffled.slice(0,n);
   var result = selected.join("");
 
-  if (n > mergedArraystep4.length) {
-    // repeat array until n < length
-    while (n > mergedArraystep4.length) {
-      mergedArraystep4 = mergedArraystep4.concat(mergedArraystep4);
-    } if ((n === mergedArraystep4.length) || (n < mergedArraystep4.length)) {
-      return result;
-    }
-  } else {
+  if (n < mergedArraystep4.length) {
     return result;
-  };
+  } else {
+    var longerArray = mergedArraystep4.concat(mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4);
+    var shuffled = longerArray.sort(function() {return 0.5 - Math.random(); });
+    var selected = shuffled.slice(0,n);
+    var result = selected.join("");
+    return result;
+  }
 }
 
 // Get references to the #generate element
