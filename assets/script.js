f1e39ee7b5a301481122f7ffe261ddf3b525dfce
unknown <evelyneidsath@gmail.com>
2023-11-18 16:15:41 -0800

    add comments to js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,15 @@
 // Assignment code here
+
+//arrays for password character types
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = lowerCase.map(function(x){return x.toUpperCase(); });
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "_", "-", "+", "=", "/"];
 
 
-
+//function to generate the password
 function generatePassword() {
+  //prompt to get password length, character types, and ensure length is between 8-128
   var userLength = prompt("Enter desired length of password between 8 and 128 characters.");
   if ((userLength < 8) || (userLength > 128)) {
     window.alert("Password length must be between 8 and 128 characters.");
@@ -17,13 +20,15 @@ function generatePassword() {
     var userUpperCase = window.confirm("Include upper case letters?");
     var userNumbers = window.confirm("Include numbers?");
     var userSpecialCharacters = window.confirm("Include special characters?");
-  } 
+  }
+  //if no types are selected, alert user
   if ((userLowerCase === false) && (userUpperCase === false) && (userNumbers === false) && (userSpecialCharacters === false)) {
     window.alert("Please select at least one character type.");
     var startOver2 = window.confirm("Start over?");;
     if (startOver2) {generatePassword();};
   }
 
+  //this is how character arrays are added or omitted during selection
   if (userLowerCase === true) {
     var mergedArraystep1 = [].concat(lowerCase);
   } else if (userLowerCase === false) {
@@ -44,15 +49,18 @@ function generatePassword() {
   } else if (userSpecialCharacters === false) {
     var mergedArraystep4 = mergedArraystep3;
   };
+
+  //defining variables for the length and merged array, merged array is sorted and sliced to pull characters randomly for password
   var n = userLength;
   var shuffled = mergedArraystep4.sort(function() {return 0.5 - Math.random(); });
   var selected = shuffled.slice(0,n);
   var result = selected.join("");
 
+  //accounting for cases where the desired password length is longer than array length
   if (n < mergedArraystep4.length) {
     return result;
   } else {
-    var longerArray = mergedArraystep4.concat(mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4);
+    var longerArray = mergedArraystep4.concat(mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4, mergedArraystep4);
     var shuffled = longerArray.sort(function() {return 0.5 - Math.random(); });
     var selected = shuffled.slice(0,n);
     var result = selected.join("");
